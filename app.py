5a13b5a219c4c1d0270746baf57f3530198948fa
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-11-20 01:09:06 +0400

    feat: add post creation (model) and SQLAlchemy

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 from flask import Flask
 from config import Configuration
-from posts.blueprint import posts
-from goods.blueprint import goods
-from tests.blueprint import tests
+from flask_sqlalchemy import SQLAlchemy
 
 # __name__ - имя текущего файла app.py
 app = Flask(__name__)
 
+# Инициализация бд
+db = SQLAlchemy(app)
+
 # Подключение конфигурации
 app.config.from_object(Configuration)
 
-# Регистрация blueprint (экземпляр класса Blueprint,путь до нашего blueprint)
-app.register_blueprint(posts, url_prefix='/blog')
-app.register_blueprint(goods, url_prefix='/goods')
-app.register_blueprint(tests, url_prefix="/tests"
-                                         "")
+
+
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from app import  app
+from app import app
 import view
+from app import db
+from posts.blueprint import posts
+
+# Регистрация blueprint (экземпляр класса Blueprint,путь до нашего blueprint)
+app.register_blueprint(posts, url_prefix='/blog')
 
 # Создание точки входа
 if __name__ == '__main__':
