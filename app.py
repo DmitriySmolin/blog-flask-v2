e4e6a92c653d90e85c7e6720053deabd5ff736b3
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-11-20 20:34:25 +0400

    Migrations, tags, linking tags and posts

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ from flask import Flask
 from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
 
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
 # __name__ - имя текущего файла app.py
 app = Flask(__name__)
 
 # Инициализация бд
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
 # Подключение конфигурации
 app.config.from_object(Configuration)
-
-
 
